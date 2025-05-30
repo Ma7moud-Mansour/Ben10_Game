ce55f41ce716535be3fbb9b1ec574d6f82287c53
Omar Hesham <omarhesham1192019@gmail.com>
2025-05-31 01:46:13 +0300

    Default Standards

--- Game/Game/Form1.cs
+++ Game/Game/Form1.cs
@@ -12,10 +12,49 @@ namespace Game
 {
     public partial class Form1 : Form
     {
+        Bitmap off;
+        Timer tt = new Timer();
         public Form1()
         {
-            // Hello
+            this.WindowState = FormWindowState.Maximized;
+            this.Paint += Form1_Paint;
+            this.Load += Form1_Load;
+            tt.Tick += Tt_Tick;
+            tt.Start();
             InitializeComponent();
+        }
+
+        private void Tt_Tick(object sender, EventArgs e)
+        {
+            DrawDubb(this.CreateGraphics());
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Create
+            // Draw
+            off = new Bitmap(this.ClientSize.Width, this.ClientSize.Height);
+            tt.Interval = 100;
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            DrawDubb(this.CreateGraphics());
+        }
+
+        void DrawDubb(Graphics g)
+        {
+            Graphics g2 = Graphics.FromImage(off);
+            DrawScene(g2);
+            g.DrawImage(off, 0, 0);
+        }
+
+        void DrawScene(Graphics g)
+        {
+            // Drawing logic goes here
+            g.Clear(Color.Black);
+            // Example: draw a simple rectangle
+            g.FillRectangle(Brushes.Red, 50, 50, 100, 100);
         }
     }
 }
