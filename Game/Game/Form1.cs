0d2df82cb991500e1452863cd227ddf12a4b7ce1
Omar Hesham <omarhesham1192019@gmail.com>
2025-05-31 23:23:01 +0300

    Edit Intro Motion & Customize Menu & Exit Option

--- Game/Game/Form1.cs
+++ Game/Game/Form1.cs
@@ -15,7 +15,9 @@ namespace Game
         Bitmap off;
         Timer IntroTimer = new Timer();
         int CurrentFrame = 0;
-        int IntroFrameCount = 330;
+        int IntroFrameCount = 165;
+        private System.Media.SoundPlayer player;
+        int CurrentMenu = -1;
 
         public Form1()
         {
@@ -26,11 +28,68 @@ namespace Game
             this.Load += Form1_Load;
             IntroTimer.Tick += IntroTimer_Tick;
             this.MouseMove += Form1_MouseMove;
+            this.MouseDown += Form1_MouseDown;
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            int w = this.ClientSize.Width;
+            int h = this.ClientSize.Height;
+            int x = e.X;
+            int y = e.Y;
+            if(CurrentMenu == 1)
+            {
+                Graphics g = this.CreateGraphics();
+                g.DrawImage(new Bitmap("Assets/Menu/Cover.jpg"), 0, 0, this.ClientSize.Width, this.ClientSize.Height);
+                System.Threading.Thread.Sleep(1000);
+                g.Clear(Color.Black);
+            }
+            else if (CurrentMenu == 2)
+            {
+
+            }
+            else if (CurrentMenu == 3)
+            {
+                Graphics g = this.CreateGraphics();
+                g.DrawImage(new Bitmap("Assets/Menu/Cover.jpg"), 0, 0, this.ClientSize.Width, this.ClientSize.Height);
+                System.Threading.Thread.Sleep(1000);
+                this.Close();
+            }
         }
 
         private void Form1_MouseMove(object sender, MouseEventArgs e)
         {
-
+            if(CurrentMenu != -1)
+            {
+                int w = this.ClientSize.Width;
+                int h = this.ClientSize.Height;
+                int x = e.X;
+                int y = e.Y;
+                if (x >= w * 5 / 64 && x <= w * 133 / 384 && y >= h * 35 / 72 && y <= h * 43 / 72)
+                {
+                    CurrentMenu = 1;
+                    Graphics g = this.CreateGraphics();
+                    g.DrawImage(new Bitmap("Assets/Menu/Menu_Hover_1.jpg"), 0, 0, this.ClientSize.Width, this.ClientSize.Height);
+                }
+                else if (x >= w * 5 / 64 && x <= w * 133 / 384 && y >= h * 17 / 27 && y <= h * 3 / 4)
+                {
+                    CurrentMenu = 2;
+                    Graphics g = this.CreateGraphics();
+                    g.DrawImage(new Bitmap("Assets/Menu/Menu_Hover_2.jpg"), 0, 0, this.ClientSize.Width, this.ClientSize.Height);
+                }
+                else if (x >= w * 5 / 64 && x <= w * 133 / 384 && y >= h * 169 / 216 && y <= h * 97 / 108)
+                {
+                    CurrentMenu = 3;
+                    Graphics g = this.CreateGraphics();
+                    g.DrawImage(new Bitmap("Assets/Menu/Menu_Hover_3.jpg"), 0, 0, this.ClientSize.Width, this.ClientSize.Height);
+                }
+                else if (CurrentMenu != 0)
+                {
+                    CurrentMenu = 0;
+                    Graphics g = this.CreateGraphics();
+                    g.DrawImage(new Bitmap("Assets/Menu/Menu.jpg"), 0, 0, this.ClientSize.Width, this.ClientSize.Height);
+                }
+            }
         }
 
         private void IntroTimer_Tick(object sender, EventArgs e)
@@ -40,6 +99,7 @@ namespace Game
             {
                 IntroTimer.Stop();
                 player.Stop();
+                CurrentMenu = 0;
                 Menu();
             }
             else
@@ -49,7 +109,6 @@ namespace Game
             }
         }
 
-        private System.Media.SoundPlayer player;
 
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -58,9 +117,9 @@ namespace Game
 
             // Start background music
             player = new System.Media.SoundPlayer("Assets/Audio/intro_music.wav");
-            player.PlayLooping(); // يشغلها في لوب طول فترة الانترو
+            player.PlayLooping();
 
-
+            // Start Drawing
             off = new Bitmap(this.ClientSize.Width, this.ClientSize.Height);
             IntroTimer.Interval = 1;
             IntroTimer.Start();
@@ -91,7 +150,7 @@ namespace Game
         void Menu()
         {
             Graphics g = this.CreateGraphics();
-            g.DrawImage(new Bitmap("Assets/Intro/Menu.jpg"), 0, 0, this.ClientSize.Width, this.ClientSize.Height);
+            g.DrawImage(new Bitmap("Assets/Menu/Menu.jpg"), 0, 0, this.ClientSize.Width, this.ClientSize.Height);
         }
     }
 }
