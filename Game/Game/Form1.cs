2ca4800cfc4228a021902f47c94c1dd22c21f03b
mahmoud <mahmoudabdelkareem.info@gmail.com>
2025-05-31 21:15:45 +0300

    Add Audio Stop Condition

--- Game/Game/Form1.cs
+++ Game/Game/Form1.cs
@@ -39,6 +39,7 @@ namespace Game
             if (CurrentFrame > IntroFrameCount)
             {
                 IntroTimer.Stop();
+                player.Stop();
                 Menu();
             }
             else
